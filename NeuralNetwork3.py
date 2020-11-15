b0fadf23e95a4506dc20193d4322ca2ea73c2f38
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-15 02:21:25 -0500

    Fetch data from training csv file

---
+++ NeuralNetwork3.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('../data/train_image.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(row)
+        line_count += 1
