f9d00d6662a65de14febc7bea02587f7cf3055a3
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-16 10:18:13 -0500

    Access data from train image and label files

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -1,9 +1,18 @@
 import csv
 
-with open('../data/train_image.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(row)
-        line_count += 1
+train_image = open('../data/train_image.csv')
+train_label = open('../data/train_label.csv')
+
+train_image_reader = csv.reader(train_image, delimiter=',')
+train_label_reader = csv.reader(train_label, delimiter=',')
+
+line_count = 0
+for row in train_image_reader:
+    if line_count == 0:
+        print(row)
+        break
+
+for label in train_label_reader:
+    if line_count == 0:
+        print(label)
+        break
