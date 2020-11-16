421ff17c320c329d030b2ac529809d2f2996ec1c
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-16 18:13:31 -0500

    Build and train a single node network

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -4,28 +4,30 @@ import numpy as np
 train_image = open('../data/train_image.csv')
 train_label = open('../data/train_label.csv')
 
-train_image_reader = csv.reader(train_image, delimiter=',')
-train_label_reader = csv.reader(train_label, delimiter=',')
-
-train_image_list = list(train_image_reader)
-train_label_list = list(train_label_reader)
-
 def sigmoid_function(z):
     return 1 / (1 + np.exp(-z))
 
 def cost_function(y, y_hat):
-    x = np.array(y)
-    x_hat = np.array(y_hat)
-    m = 1
-    return -(1./m) * (np.sum(np.multiply(np.log(x_hat), x)) + np.sum(np.multiply(np.log(1-x_hat), (1-x))))
+    m = y.shape[1]
+    return -(1./m) * (np.sum(np.multiply(np.log(y_hat), y)) + np.sum(np.multiply(np.log(1-y_hat), (1-y))))
 
 learning_rate = 1
 
-X = np.array(list(map(int, train_image_list[0])))
-Y = np.array(list(map(int, train_label_list[0])))
+X = np.genfromtxt('../data/train_image.csv', delimiter=',')
+Y = np.genfromtxt('../data/train_label.csv', delimiter=',')
+
+Y = np.reshape(Y, (-1, 1))
+
+X = (X/255).T
+
+y_new = np.zeros(Y.shape)
+y_new[np.where(Y == 0.0)[0]] = 1
+Y = y_new
+
+Y = Y.T
 
 n_x = X.shape[0]
-m = 1
+m = X.shape[1]
 
 W = np.random.randn(n_x, 1) * 0.01
 b = np.zeros((1, 1))
