a012e0c96ad0e19fea000896b0c4f98e6ab9e6d2
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-16 12:34:52 -0500

    Convert image and label rows into int np arrays

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -7,16 +7,21 @@ train_label = open('../data/train_label.csv')
 train_image_reader = csv.reader(train_image, delimiter=',')
 train_label_reader = csv.reader(train_label, delimiter=',')
 
+train_image_list = list(train_image_reader)
+train_label_list = list(train_label_reader)
+
 def sigmoid_function(z):
     return 1 / (1 + np.exp(-z))
 
-line_count = 0
-for row in train_image_reader:
-    if line_count == 0:
-        print(row)
-        break
+def cost_function(y, y_hat):
+    x = np.array(y)
+    x_hat = np.array(y_hat)
+    m = x.shape[1]
+    return -(1./m) * (np.sum(np.multiply(np.log(x_hat), x)) + np.sum(np.multiply(np.log(1-x_hat), (1-x))))
 
-for label in train_label_reader:
-    if line_count == 0:
-        print(label)
-        break
+for row in range(2000):
+    int_row = list(map(int, train_image_list[row]))
+    print(np.reshape(np.array(int_row), (28, 28)))
+    int_label = list(map(int, train_label_list[row]))
+    print(int_label)
+    break
