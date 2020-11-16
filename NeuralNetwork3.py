d13d88985f15a90fe2885a16396c23fad501cff0
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-16 11:19:20 -0500

    Create the sigmoid function

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -1,10 +1,14 @@
 import csv
+import numpy as np
 
 train_image = open('../data/train_image.csv')
 train_label = open('../data/train_label.csv')
 
 train_image_reader = csv.reader(train_image, delimiter=',')
 train_label_reader = csv.reader(train_label, delimiter=',')
+
+def sigmoid_function(z):
+    return 1 / (1 + np.exp(-z))
 
 line_count = 0
 for row in train_image_reader:
