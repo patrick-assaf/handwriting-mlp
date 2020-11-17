8ea19b24ca7444e08b95265fd6cbc80e68aa0ee7
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-17 00:01:19 -0500

    Create one hidden layer for the neural network

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -11,8 +11,6 @@ def cost_function(y, y_hat):
     m = y.shape[1]
     return -(1./m) * (np.sum(np.multiply(np.log(y_hat), y)) + np.sum(np.multiply(np.log(1-y_hat), (1-y))))
 
-learning_rate = 1
-
 X = np.genfromtxt('../data/train_image.csv', delimiter=',')
 Y = np.genfromtxt('../data/train_label.csv', delimiter=',')
 
@@ -27,26 +25,41 @@ Y = y_new
 Y = Y.T
 
 n_x = X.shape[0]
-m = X.shape[1]
+n_h = 64
+learning_rate = 1
 
-W = np.random.randn(n_x, 1) * 0.01
-b = np.zeros((1, 1))
+W1 = np.random.randn(n_h, n_x)
+b1 = np.zeros((n_h, 1))
+W2 = np.random.randn(1, n_h)
+b2 = np.zeros((1, 1))
 
+m = 60000
 cost = 0
 
 for i in range(2000):
-    Z = np.matmul(W.T, X) + b
-    A = sigmoid_function(Z)
 
-    cost = cost_function(Y, A)
+    Z1 = np.matmul(W1, X) + b1
+    A1 = sigmoid_function(Z1)
+    Z2 = np.matmul(W2, A1) + b2
+    A2 = sigmoid_function(Z2)
 
-    dW = (1/m) * np.matmul(X, (A-Y).T)
-    db = (1/m) * np.sum(A-Y, axis=1, keepdims=True)
+    cost = cost_function(Y, A2)
 
-    W = W - learning_rate * dW
-    b = b - learning_rate * db
+    dZ2 = A2-Y
+    dW2 = (1./m) * np.matmul(dZ2, A1.T)
+    db2 = (1./m) * np.sum(dZ2, axis=1, keepdims=True)
 
-    if (i % 100 == 0):
+    dA1 = np.matmul(W2.T, dZ2)
+    dZ1 = dA1 * sigmoid_function(Z1) * (1 - sigmoid_function(Z1))
+    dW1 = (1./m) * np.matmul(dZ1, X.T)
+    db1 = (1./m) * np.sum(dZ1, axis=1, keepdims=True)
+
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+
+    if i % 100 == 0:
         print("Epoch", i, "cost: ", cost)
 
 print("Final cost:", cost)
