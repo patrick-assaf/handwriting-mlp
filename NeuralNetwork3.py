7a4fd4ee3db4155b0c87149045b66f37963f7e71
Patrick Assaf <patrickassaf96@gmail.com>
2020-11-18 14:59:45 -0500

    Change data structure of parameters

--- NeuralNetwork3.py
+++ NeuralNetwork3.py
@@ -32,12 +32,10 @@ train_labels = process_train_labels(train_labels)
 
 # initialization
 np.random.seed(138)
-parameters = { 
-    "W1": np.random.randn(64, 784) * np.sqrt(1/784),
-    "b1": np.zeros((64, 1)) * np.sqrt(1/784),
-    "W2": np.random.randn(digits, 64) * np.sqrt(1/64),
-    "b2": np.zeros((digits, 1)) * np.sqrt(1/64) 
-}
+W1 = np.random.randn(64, 784) * np.sqrt(1/784)
+b1 = np.zeros((64, 1)) * np.sqrt(1/784)
+W2 = np.random.randn(digits, 64) * np.sqrt(1/64)
+b2 = np.zeros((digits, 1)) * np.sqrt(1/64) 
 
 gradients = {"dW1": 0, "db1": 0, "dW2": 0, "db2": 0}
 
@@ -52,20 +50,20 @@ def sigmoid_function(x):
 def cost_function(x, x_hat):
     return -(1/x.shape[1]) * (np.sum(np.multiply(x, np.log(x_hat))) + np.sum(np.multiply((1-x), np.log(1-x_hat))))
 
-def feedforward(image_data, parameters):
+def feedforward(image_data, W1, b1, W2, b2):
     cache = {}
-    cache["Z1"] = np.matmul(parameters["W1"], image_data) + parameters["b1"]
+    cache["Z1"] = np.matmul(W1, image_data) + b1
     cache["A1"] = sigmoid_function(cache["Z1"])
-    cache["Z2"] = np.matmul(parameters["W2"], cache["A1"]) + parameters["b2"]
+    cache["Z2"] = np.matmul(W2, cache["A1"]) + b2
     cache["A2"] = np.exp(cache["Z2"]) / np.sum(np.exp(cache["Z2"]), axis=0)
     return cache
 
-def backpropagation(image_data, labels, parameters, cache):
+def backpropagation(image_data, labels, cache, W2):
     dZ2 = cache["A2"] - labels
     dW2 = (1/current_batch_size) * np.matmul(dZ2, cache["A1"].T)
     db2 = (1/current_batch_size) * np.sum(dZ2, axis=1, keepdims=True)
 
-    dA1 = np.matmul(parameters["W2"].T, dZ2)
+    dA1 = np.matmul(W2.T, dZ2)
     dZ1 = dA1 * sigmoid_function(cache["Z1"]) * (1 - sigmoid_function(cache["Z1"]))
     dW1 = (1/current_batch_size) * np.matmul(dZ1, image_data.T)
     db1 = (1/current_batch_size) * np.sum(dZ1, axis=1, keepdims=True)
@@ -84,22 +82,22 @@ for epoch in range(9):
         image_data = train_image_data[:, batch_start:batch_end]
         labels = train_labels[:, batch_start:batch_end]
 
-        cache = feedforward(image_data, parameters)
-        gradients = backpropagation(image_data, labels, parameters, cache)
+        cache = feedforward(image_data, W1, b1, W2, b2)
+        gradients = backpropagation(image_data, labels, cache, W2)
 
         V_dW1 = (beta * V_dW1 + (1 - beta) * gradients["dW1"])
         V_db1 = (beta * V_db1 + (1 - beta) * gradients["db1"])
         V_dW2 = (beta * V_dW2 + (1 - beta) * gradients["dW2"])
         V_db2 = (beta * V_db2 + (1 - beta) * gradients["db2"])
 
-        parameters["W1"] = parameters["W1"] - learning_rate * V_dW1
-        parameters["b1"] = parameters["b1"] - learning_rate * V_db1
-        parameters["W2"] = parameters["W2"] - learning_rate * V_dW2
-        parameters["b2"] = parameters["b2"] - learning_rate * V_db2
+        W1 = W1 - learning_rate * V_dW1
+        b1 = b1 - learning_rate * V_db1
+        W2 = W2 - learning_rate * V_dW2
+        b2 = b2 - learning_rate * V_db2
 
-    cache = feedforward(train_image_data, parameters)
+    cache = feedforward(train_image_data, W1, b1, W2, b2)
     train_cost = cost_function(train_labels, cache["A2"])
-    cache = feedforward(test_image_data, parameters)
+    cache = feedforward(test_image_data, W1, b1, W2, b2)
     print("Epoch {}: training cost = {}".format(epoch+1 ,train_cost))
 
 print("Done.")
@@ -114,7 +112,7 @@ Y_test_new = np.eye(digits)[Y_test.astype('int32')]
 Y_test_new = Y_test_new.T.reshape(digits, test_examples)
 Y_test = Y_test_new[:,:sample_size]
 
-cache = feedforward(test_image_data, parameters)
+cache = feedforward(test_image_data, W1, b1, W2, b2)
 predictions = np.argmax(cache["A2"], axis=0)
 labels = np.argmax(Y_test, axis=0)
 
